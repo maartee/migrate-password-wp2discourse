637a8dcab110c8c99d4d17f89acc6212f1687f56
Martin Ovsenak <xmarty@email.cz>
2020-11-12 14:05:13 +0000

    Create pwimport.sh

---
+++ pwimport.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+inputfile=$1
+
+for line in $(cat $inputfile);do
+echo "LINE:  $line"
+  email=$(echo  $line | cut -d';' -f1)
+  hashedpw=$(echo  $line | cut -d';' -f2)
+    echo "email: $email, hashedpw: $hashedpw"
+    echo -e "u = User.find_by_email('$email'); 0\nu.custom_fields['import_pass'] = '$hashedpw'\nu.save" | rails c
+done
