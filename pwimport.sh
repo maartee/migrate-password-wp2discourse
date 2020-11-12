f16cb44dcee4e2d84c878be8812cb629a624f7b8
Martin Ovsenak <xmarty@email.cz>
2020-11-12 18:15:43 +0000

    speed up import

--- pwimport.sh
+++ pwimport.sh
@@ -7,5 +7,6 @@ echo "LINE:  $line"
   email=$(echo  $line | cut -d';' -f1)
   hashedpw=$(echo  $line | cut -d';' -f2)
     echo "email: $email, hashedpw: $hashedpw"
-    echo -e "u = User.find_by_email('$email'); 0\nu.custom_fields['import_pass'] = '$hashedpw'\nu.save" | rails c
+    echo -e "u = User.find_by_email('$email'); 0\nu.custom_fields['import_pass'] = '$hashedpw'\nu.save" >>  import.rb
 done
+cat import.rb | rails c
