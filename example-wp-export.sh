c830ea094fd366ca8cc6ed124fe573857664316e
Martin Ovsenak <xmarty@email.cz>
2020-11-12 14:18:05 +0000

    Update example-wp-export.sh

--- example-wp-export.sh
+++ example-wp-export.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-mysql -p<dbuser> -p<dbpwd> <dbname> -e 'select user_login,user_pass from wp_users; ' > <outputfile>
+mysql -p<dbuser> -p<dbpwd> <dbname> -NB -e 'select user_login,user_pass from wp_users; ' > <outputfile>
